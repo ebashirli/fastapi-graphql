84d9cc56f51cea1e453faf484ae6e4e853c2e8a4
ebashirli <ebashirli@gmail.com>
2022-08-11 17:15:56 +0400

    feat: add tests and updated .gitignore file

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from starlette.graphql import GraphQLApp
 
 from schema import Query, Mutation
 
+import collections
+collections_abc = getattr(collections, 'abc', collections)
+
 app = FastAPI()
 
 app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query, mutation=Mutation)))
