e4d1123720a6e66b287cae92d5a38f8b94ca9210
ebashirli <ebashirli@gmail.com>
2022-08-11 16:37:39 +0400

    fix: add forgotten method

--- schema.py
+++ schema.py
@@ -15,11 +15,22 @@ from models.user import User
 
 
 class Query(graphene.ObjectType):
+    
     say_hello = graphene.String(name=graphene.String(default_value='Test Driven'))
+    list_users = graphene.List(UserGrapheneModel)
+    get_single_user = graphene.Field(UserGrapheneModel, user_id=graphene.NonNull(graphene.Int))
 
     @staticmethod
     def resolve_say_hello(parent, info, name):
         return f'Hello {name}'
+
+    @staticmethod
+    def resolve_list_users(parent, info):
+        return User.all()
+    
+    @staticmethod
+    def resolve_get_single_user(parent, info, user_id):
+        return User.find_or_fail(user_id)
 
 
 class CreateUser(graphene.Mutation):
