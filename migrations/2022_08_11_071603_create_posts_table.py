3773a42ea39c7a0e9bd4442d87b092c84cecae13
Elvin Bashirli <ebashirli@gmail.com>
2022-08-11 08:53:03 +0000

    fix: fix indentation

--- migrations/2022_08_11_071603_create_posts_table.py
+++ migrations/2022_08_11_071603_create_posts_table.py
@@ -8,12 +8,12 @@ class CreatePostsTable(Migration):
         Run the migrations.
         """
         with self.schema.create('posts') as table:
-		table.increments('id')
-		table.integer('user_id').unsigned()
-	        table.foreign('user_id').references('id').on('users')
-        	table.string('title')
-	        table.text('body')
-        	table.timestamps()
+            table.increments('id')
+            table.integer('user_id').unsigned()
+            table.foreign('user_id').references('id').on('users')
+            table.string('title')
+            table.text('body')
+            table.timestamps()
 
     def down(self):
         """
